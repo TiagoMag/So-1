ec822f8163b3e5ea78b43399052b1fd3fe845e01
Duarte Vilar <48862635+mishlawi420@users.noreply.github.com>
2019-04-30 17:39:17 +0100

    Add files via upload

--- cv.c
+++ cv.c
@@ -18,29 +18,40 @@ struct stock{
  int quant;
 };
 
+//retornar a quantidade em stock e o preco de um artigo
+
+
+
+
 int main (int argc,char** argv){ 
  struct cliente aux;
  struct stock aux2;
- char Buffer2[100];
+ char Buffer2[100]; 
  char buff[PIPE_BUF];
- sprintf(buff,"%s%d","fifo",(int)getpid());
- mkfifo(buff,0666);
- char buffer[50];
- int m,d,n;
- aux.pid=(int)getpid();
- while(read(0,buffer,50)){
-  int fifo=open("fifo", O_WRONLY);
-  strcpy(aux.buffer,buffer);
-  write(fifo,&aux,sizeof(struct cliente));
-  int clientfd=open(buff,O_RDONLY);
-  read(clientfd,&d,sizeof(d));
-  snprintf(Buffer2,sizeof(int),"%d",d);
-  write(1,Buffer2,strlen(Buffer2));
-  char *newline=strdup("\n");
-  write(1,newline,2);
-  close(fifo);
+    sprintf(buff,"%s%d","fifo",(int)getpid()); //passa um int para char
+    mkfifo(buff,0666);
+char buffer[50]; //buffer que depois vai escrever no buffer do cliente
+int d;
+aux.pid=(int)getpid();
+     while(read(0,buffer,50)){
+int fd_fifo=open("fifo", O_WRONLY);
+    strcpy(aux.buffer,buffer); // escrever no buffer do cliente o do buffer anterior
+    write(fd_fifo,&aux,sizeof(struct cliente));
+    int clientfd=open(buff,O_RDONLY);
+    read(clientfd,&d,sizeof(d));
+    snprintf(Buffer2,sizeof(int),"%d",d); //int to string
+    write(1,Buffer2,strlen(Buffer2)); //write no  sdtdout
+    char *newline=strdup("\n");
+    write(1,newline,2);
+    close(fd_fifo);
+}
+//retornar a quantidade em stock e o preco de um artigo
+//int fd_stock = open ("STOCKS.txt",O_RDONLY);
+//float procurapreco (struct artigo *aux1,int fd2,int codigo)
 
- }
- //close(fifo);
+
+
+
+//close(fifo);
  return 0;
 }
